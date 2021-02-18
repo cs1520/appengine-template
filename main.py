f29958cb4e6f98ad6dde5dc66b0168968de0299d
Jake Stambaugh <jake.stambaugh@gmail.com>
2021-02-17 20:00:10 -0500

    Change a few syntax errors and imports

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-app.route("/")
+@app.route("/")
 def home():
     """Return a simple HTML page."""
+    print("Hit the route!")
     return render_template("index.html")
 
 
